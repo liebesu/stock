2d991f527e00dcd6828a530a2d067f0a7189f598
liebesu <lichunjiang42@gmail.com>
2016-04-12 18:15:37 +0800

    update
    update

--- apix.py
+++ apix.py
@@ -3,21 +3,29 @@ import json
 import datetime
 __author__ = 'admin'
 import requests
-start=time.clock()
-url = "http://a.apix.cn/apixmoney/stockdata/stock"
 
-querystring = {"stockid":"sz000670"}
+def stock():
+    start=time.clock()
+    url = "http://a.apix.cn/apixmoney/stockdata/stock"
+    global relprice
+    querystring = {"stockid":"sz000670"}
 
-headers = {
-    'accept': "application/json",
-    'content-type': "application/json",
-    'apix-key': "2583488c206b4d2e654d9db7add7fdab"
-    }
+    headers = {
+        'accept': "application/json",
+        'content-type': "application/json",
+        'apix-key': "2583488c206b4d2e654d9db7add7fdab"
+        }
+    relprice=0
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    print datetime.datetime.now()
+    price=json.loads(response.text)['data']['stockinfo']['auctionPrice']
 
-response = requests.request("GET", url, headers=headers, params=querystring)
-print datetime.datetime.now()
-print json.loads(response.text)['data']['stockinfo']['auctionPrice']
+    if relprice!=price:
+        print price
+        relprice=price
 
-
-end=time.clock()
-print end-start
+    end=time.clock()
+    print end-start
+if __name__=="__main__":
+    for i in range(10000):
+        stock()
